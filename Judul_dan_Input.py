d86d01869f5487c2f74761167f2819e877d2b76e
reza manazil <rezamanazil@gmail.com>
2022-11-03 20:52:21 +0700

    judul dan input

---
+++ Judul_dan_Input.py
@@ -0,0 +1,8 @@
+# title aja
+print("\n\t==============================")
+print("\t==== KALKULATOR SEDERHANA ====")
+print("\t==============================\n")
+
+# Proses input nilai/data
+nilai1=int(input("Masukan Angka Pertama \t: "))
+nilai2=int(input("Masukan Angka Kedua \t: "))
