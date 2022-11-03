3f34dbf56c0bc8b280ce78ba1c1c646fa8fa343a
fadilahputri <fadilaahputrisukoco@gmail.com>
2022-11-03 21:06:23 +0700

    nambahin proses perhitungan perkalian dan pembagian

--- kalkulator_kabagtakur.py
+++ kalkulator_kabagtakur.py
@@ -19,3 +19,14 @@ pilih=int(input("Pilih Proses Perhitungan : "))
 if(pilih == 1):
     nilai3 = nilai1+nilai2
     print("Hasil Penjumlahan ",nilai1,"dan ",nilai2," adalah ",nilai3)
+elif(pilih == 2):
+    nilai3 = nilai1-nilai2
+    print("Hasil Pengurangan ",nilai1," dan ",nilai2," adalah ",nilai3)
+elif(pilih == 3):
+    nilai3 = nilai1*nilai2
+    print("Hasil Perkalian ",nilai1," dan ",nilai2," adalah ",nilai3)
+elif(pilih == 4):
+    nilai3 = nilai1/nilai2
+    print("Hasil Pembagian ",nilai1," dan ",nilai2," adalah ",nilai3)
+else:
+    print("Maaf Pilihan Anda tidak tersedia")
