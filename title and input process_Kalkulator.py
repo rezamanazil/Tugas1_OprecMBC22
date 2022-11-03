d5c417a4f1ca5e301cd6de66956fa8f8f08d0911
Galang Aditya <galang11183@gmail.com>
2022-11-03 20:39:45 +0700

    update nambahin menu dan operasi perhitungan

--- title and input process_Kalkulator.py
+++ title and input process_Kalkulator.py
@@ -7,3 +7,15 @@ print("\t==============================\n")
 nilai1=int(input("Masukan Angka Pertama \t: "))
 nilai2=int(input("Masukan Angka Kedua \t: "))
 
+# Menu Pilihan
+print("\n1. Penjumlahan\n")
+print("2. Pengurangan\n")
+print("3. Perkalian\n")
+print("4. Pembagian\n")
+
+#Proses pemilihan proses perhitungan
+pilih=int(input("Pilih Proses Perhitungan : "))
+
+if(pilih == 1):
+    nilai3 = nilai1+nilai2
+    print("Hasil Penjumlahan ",nilai1,"dan ",nilai2," adalah ",nilai3)
